eed382ccd63e8c5b3aa5dea7ba120b17873448b9
Y samson <ysamsoncts@gmail.com>
2026-02-16 18:25:50 +0530

    CREATED INITIAL FOLDER STRUCTURE

--- src/main/java/org/example/Ak.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class Ak {
-    public static void main(String[] args){
-        System.out.println("Hello from Ak");
-    }
-}
--- src/main/java/org/example/Samidha.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Samidha {
-
-   public static void main(String[] args) {
-//jguyg
-    }
-}
