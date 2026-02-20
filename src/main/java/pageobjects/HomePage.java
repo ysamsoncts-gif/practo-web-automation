1998f3275af6fbe631ee2b98fc48a6fea034af78
Ak <akcodes27@gmail.com>
2026-02-20 14:17:31 +0530

    added Test case 02 for validating hospitals in Bangalore/JP Nagar

--- src/main/java/pageobjects/HomePage.java
+++ src/main/java/pageobjects/HomePage.java
@@ -1,13 +1,9 @@
 package pageobjects;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import utilities.WaitUtils;
-
-import java.time.Duration;
 import java.util.List;
 
 public class HomePage {
@@ -15,62 +11,75 @@ public class HomePage {
     private final WebDriver driver;
     private final WaitUtils wait;
 
-    public By locationSearchBar = By.xpath("//input[@placeholder='Search location']");
-    public By searchHospital = By.xpath("//input[@placeholder='Search doctors, clinics, hospitals, etc.']");
-    public By hospitalName = By.xpath("//h2[@class='line-1']");
-    public By hospitalRate = By.xpath("//div[@class='text-1']/child::span[@class='u-bold']");
-    public By hospitalTime = By.xpath("//span[@class='pd-right-2px-text-green']");
+    public By hospitalSearchBarLocator = By.xpath("//input[@placeholder='Search doctors, clinics, hospitals, etc.']");
+    public By hospitalOptionLocator = By.xpath("(//div[contains(text(),\"Hospital\")])[4]");
+
+    public By locationSearchBarLocator = By.xpath("//input[@placeholder='Search location']");
+    public By BangaloreOptionLocator = By.xpath("(//div[contains(text(),\"Bangalore\")])");
+    public By JPNagarOptionLocator = By.xpath("(//div[contains(text(),\"Jp Nagar\")])");
+
+    public By resultHospitalLocationLocator = By.xpath("//span[contains(text(),\"JP Nagar\" )]");
+
+    public By resultHospitalNameLocator = By.xpath("//h2[@class='line-1']");
+    public By resultHospitalRateLocator = By.xpath("//div[@class='text-1']/child::span[@class='u-bold']");
+    public By resultHospitalTimeLocator = By.xpath("//span[@class='pd-right-2px-text-green']");
 
 
-    public HomePage(WebDriver driver){
+    public HomePage(WebDriver driver) {
         this.driver = driver;
         this.wait = new WaitUtils(driver, 20);
     }
 
-    public String getPageTitle(){
+    public String getPageTitle() {
         return driver.getTitle();
     }
 
-    public void searchLocation(String location){
-        Actions actions = new Actions(driver);
-        WebElement sendLocation = driver.findElement(locationSearchBar);
-        sendLocation.clear();
-        sendLocation.sendKeys(location);
-    }
-
-    public void searchHospitalClinic(String hospital) {
-        Actions actions = new Actions(driver);
-        WebElement sendHospital = driver.findElement(searchHospital);
+    public void selectSearchHospitalClinic(String hospital) {
+        WebElement sendHospital = driver.findElement(hospitalSearchBarLocator);
+        sendHospital.click();
         sendHospital.sendKeys(hospital);
-        actions.click(sendHospital)
-                .pause(Duration.ofMillis(2000))
-                .sendKeys(Keys.ARROW_DOWN)
-                .sendKeys(Keys.ARROW_DOWN)
-                .sendKeys(Keys.ARROW_DOWN)
-                .sendKeys(Keys.ARROW_DOWN)
-                .sendKeys(Keys.ENTER)
-                .sendKeys(Keys.RETURN)
-                .build()
-                .perform();
-
+        wait.clickable(hospitalOptionLocator).click();
     }
 
-    public void getHospitalName(){
-        List <WebElement> hospitalNames = driver.findElements(hospitalName);
-        for(WebElement names:hospitalNames){
+    public void selectSearchLocation(String location)  {
+        WebElement sendSearchLocation = driver.findElement(locationSearchBarLocator);
+        sendSearchLocation.clear();
+        sendSearchLocation.click();
+        sendSearchLocation.sendKeys(location);
+        wait.clickable(BangaloreOptionLocator).click();
+    }
+
+    public void selectSubSearchLocation(String location)  {
+        WebElement sendSubLocation = driver.findElement(locationSearchBarLocator);
+        sendSubLocation.clear();
+        sendSubLocation.click();
+        sendSubLocation.sendKeys(location);
+        wait.clickable(JPNagarOptionLocator).click();
+        wait.present(resultHospitalLocationLocator);
+    }
+
+    public boolean verifyHospitalName() {
+        wait.present(resultHospitalLocationLocator);
+        WebElement hospitalNameLocation = driver.findElement(resultHospitalLocationLocator);
+        return hospitalNameLocation.getText().startsWith("JP Nagar");
+    }
+
+    public void storeHospitalName() {
+        List<WebElement> hospitalNames = driver.findElements(resultHospitalNameLocator);
+        for (WebElement names : hospitalNames) {
             System.out.println(names.getText());
         }
     }
 
-    public void getHospitalRating(){
-        List <WebElement> hospitalRatings = driver.findElements(hospitalRate);
+    public void storeHospitalRating(){
+        List <WebElement> hospitalRatings = driver.findElements(resultHospitalRateLocator);
         for( WebElement rates:hospitalRatings ){
             System.out.println(rates.getText());
         }
     }
 
-    public void getHospitalTiming(){
-        List <WebElement> hospitalTimings = driver.findElements(hospitalTime);
+    public void storeHospitalTiming(){
+        List <WebElement> hospitalTimings = driver.findElements(resultHospitalTimeLocator);
         for(WebElement times:hospitalTimings){
             System.out.println(times.getText());
         }
