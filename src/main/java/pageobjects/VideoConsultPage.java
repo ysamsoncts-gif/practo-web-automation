43cdc384c3ac7e2918ab89d48b82ee03262b43d6
Y samson <paridhikcts@gmail.com>
2026-03-05 15:51:42 +0530

    removed comments, unused methods and extra line spaces.

--- src/main/java/pageobjects/VideoConsultPage.java
+++ src/main/java/pageobjects/VideoConsultPage.java
@@ -1,13 +1,10 @@
 package pageobjects;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.CommonCode;
-import utilities.ScreenshotUtil;
 
 public class VideoConsultPage {
     private WebDriver driver;
@@ -68,7 +65,5 @@ public class VideoConsultPage {
         wait.visible(frame);
         driver.switchTo().frame(frame);
         return wait.visible(otpMsg).getText();
-
     }
-
 }
